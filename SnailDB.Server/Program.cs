b1800ed52df22621d647fff26880e618bb12a887
Austin <17032781@mstc.edu>
2024-07-24 21:40:55 -0500

    Re-Seeded relics and their images. Discovered that I broke relic sources so I will need to fix that.

--- SnailDB.Server/Program.cs
+++ SnailDB.Server/Program.cs
@@ -21,6 +21,8 @@ app.UseStaticFiles();
 }*/
 app.UseHttpsRedirection();
 
+// For use in local non Docker environment
+
 /*using (var scope = app.Services.CreateScope()) {
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<AppDbContext>();
